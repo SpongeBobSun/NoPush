456cd2f80355927ce89663318172688ab7e5248e
Bob Sun <widekuan@outlook.com>
2015-08-02 13:07:44 +0800

    Fix compile error on Android Studio 1.3 & NDK r10e.

--- nopush/src/main/jni/include/log.h
+++ nopush/src/main/jni/include/log.h
@@ -5,5 +5,5 @@
 
 #define LOG_TAG "NDK Log"
 #define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
-#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,"%s",__VA_ARGS__)
 #endif
